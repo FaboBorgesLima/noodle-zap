2ede2ef826dc4db7e2fb02729051ae8e787b6ee0
FaboBorgesLima <fabo.borges.lima@gmail.com>
2024-06-20 20:44:08 -0300

    updating dot env and chaging password and token length

--- db/mysql/scripts/init.sql
+++ db/mysql/scripts/init.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS user (
   user_id INT NOT NULL AUTO_INCREMENT,
   email VARCHAR(255) NOT NULL UNIQUE,
   user_name VARCHAR(255) NOT NULL,
-  user_password CHAR(64) NOT NULL,
-  token CHAR(64) NOT NULL,
+  user_password CHAR(44) NOT NULL,
+  token CHAR(44) NOT NULL,
   PRIMARY KEY(user_id)
 );
