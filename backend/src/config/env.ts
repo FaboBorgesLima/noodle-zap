d17569c494abd02bc618e62d653b34cd7cc30a0b
FaboBorgesLima <fabo.borges.lima@gmail.com>
2024-08-01 13:47:21 -0300

    adding custom error for env vars in backend

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,5 +1,3 @@
-export const env = <EnvInterface>(<unknown>process.env);
-
 export interface EnvInterface {
     MONGO_INITDB_DATABASE: string;
     MONGO_INITDB_USERNAME: string;
@@ -10,4 +8,51 @@ export interface EnvInterface {
     HMAC_KEY: string;
     TZ?: string;
     IS_PROD?: string;
+}
+class EnvVarError extends Error {
+    constructor(envVar: string, val: any) {
+        super(`env var: ${envVar} has the value '${val}' , please fix it`);
+    }
+}
+export const env = getEnv();
+
+function getEnv(): EnvInterface {
+    // mongodb vars
+    if (!process.env.MONGO_INITDB_DATABASE) {
+        throw new EnvVarError(
+            "MONGO_INITDB_DATABASE",
+            process.env.MONGO_INITDB_DATABASE
+        );
+    }
+    if (!process.env.MONGO_INITDB_USERNAME) {
+        throw new EnvVarError(
+            "MONGO_INITDB_DATABASE",
+            process.env.MONGO_INITDB_USERNAME
+        );
+    }
+    if (!process.env.MONGO_INITDB_PASSWORD) {
+        throw new EnvVarError(
+            "MONGO_INITDB_PASSWORD",
+            process.env.MONGO_INITDB_PASSWORD
+        );
+    }
+    // mysql vars
+    if (!process.env.MYSQL_DATABASE) {
+        throw new EnvVarError("MYSQL_DATABASE", process.env.MYSQL_DATABASE);
+    }
+    if (!process.env.MYSQL_USER) {
+        throw new EnvVarError("MYSQL_USER", process.env.MYSQL_USER);
+    }
+    if (!process.env.MYSQL_PASSWORD) {
+        throw new EnvVarError("MYSQL_PASSWORD", process.env.MYSQL_PASSWORD);
+    }
+    // backend vars
+    if (!process.env.HMAC_KEY) {
+        throw new EnvVarError("HMAC_KEY", process.env.HMAC_KEY);
+    }
+    if (!process.env.IS_PROD) {
+        throw new EnvVarError("IS_PROD", process.env.IS_PROD);
+    }
+
+    return <EnvInterface>(<unknown>process.env);
 }
